a9640d59d94b795b331e923ef8463df9c81377cf
Simon Shahriveri <simons@propcom.co.uk>
2014-08-12 14:20:44 +0100

    [rejig]

---
+++ httpdocs/index.php
@@ -0,0 +1,245 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<meta name="revisit-after" content="14 days" />
+		<meta name="robots" content="all" />
+		<meta name="generator" content="" />
+		<meta name="author" content="" />
+		<link rel="canonical" href="" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link rel="shortcut icon" href="/icon.png">
+		<link rel="apple-touch-icon-precomposed" href="/icon.png">
+
+		<link rel="stylesheet" href="assets/css/main.css">
+		<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+
+	</head>
+	<body id="home-page" data-page="home">
+
+		<main class="page  page--home" role="main">
+
+			<div class="container">
+
+				<div class="row">
+					<div class="col col--1 mq-tab-lrg--quart  mq-mob-lrg--half"><p>1 col</p></div>
+					<div class="col col--1 mq-tab-lrg--quart  mq-mob-lrg--half"><p>1 col</p></div>
+					<div class="col col--1 mq-tab-lrg--quart  mq-mob-lrg--half"><p>1 col</p></div>
+					<div class="col col--1 mq-tab-lrg--quart  mq-mob-lrg--half"><p>1 col</p></div>
+					<div class="col col--1 mq-tab-lrg--quart  mq-mob-lrg--half"><p>1 col</p></div>
+					<div class="col col--1 mq-tab-lrg--quart  mq-mob-lrg--half"><p>1 col</p></div>
+					<div class="col col--1 mq-tab-lrg--quart  mq-mob-lrg--half"><p>1 col</p></div>
+					<div class="col col--1 mq-tab-lrg--quart  mq-mob-lrg--half"><p>1 col</p></div>
+					<div class="col col--1 mq-tab-lrg--quart  mq-mob-lrg--half"><p>1 col</p></div>
+					<div class="col col--1 mq-tab-lrg--quart  mq-mob-lrg--half"><p>1 col</p></div>
+					<div class="col col--1 mq-tab-lrg--quart  mq-mob-lrg--half"><p>1 col</p></div>
+					<div class="col col--1 mq-tab-lrg--quart  mq-mob-lrg--half"><p>1 col</p></div>
+				</div>
+
+				<div class="row">
+					<div class="col col--2  mq-tab--half"><p>2 cols</p></div>
+					<div class="col col--2  mq-tab--half"><p>2 cols</p></div>
+					<div class="col col--2  mq-tab--half"><p>2 cols</p></div>
+					<div class="col col--2  mq-tab--half"><p>2 cols</p></div>
+					<div class="col col--2  mq-tab--half"><p>2 cols</p></div>
+					<div class="col col--2  mq-tab--half"><p>2 cols</p></div>
+				</div>
+
+				<div class="row">
+					<div class="col col--3  mq-tab--half"><p>3 cols</p></div>
+					<div class="col col--3  mq-tab--half"><p>3 cols</p></div>
+					<div class="col col--3  mq-tab--half"><p>3 cols</p></div>
+					<div class="col col--3  mq-tab--half"><p>3 cols</p></div>
+				</div>
+
+				<div class="row">
+					<div class="col col--4  mq-mob-lrg--full"><p>4 cols</p></div>
+					<div class="col col--4  mq-mob-lrg--full"><p>4 cols</p></div>
+					<div class="col col--4  mq-mob-lrg--full"><p>4 cols</p></div>
+				</div>
+
+				<div class="row">
+					<div class="col col--6"><p>6 cols</p></div>
+					<div class="col col--6"><p>6 cols</p></div>
+				</div>
+
+				<div class="row">
+					<div class="col col--12"><p>12 col</p></div>
+				</div>
+
+
+
+				<div class="row">
+					<div class="col col--6 col--center"><p>6 col center</p></div>
+				</div>
+
+				<div class="row">
+					<div class="col col--4 col--center"><p>4 col center</p></div>
+				</div>
+
+				<div class="row">
+					<div class="col col--2 col--center"><p>2 col center</p></div>
+				</div>
+
+
+
+
+				<div class="row">
+					<div class="col col--6 col--right"><p>6 col right</p></div>
+				</div>
+
+				<div class="row">
+					<div class="col col--6 col--left"><p>6 col left</p></div>
+				</div>
+
+
+
+
+				<div class="row">
+					<div class="col col--12"><p>12 cols</p></div>
+				</div>
+
+				<div class="row">
+					<div class="col col--11 col--offset-1"><p>11 cols - offset by 1 col</p></div>
+				</div>
+
+				<div class="row">
+					<div class="col col--10 col--offset-2"><p>10 cols - offset by 2 cols</p></div>
+				</div>
+
+				<div class="row">
+					<div class="col col--9 col--offset-3"><p>9 cols - offset by 3 cols</p></div>
+				</div>
+
+				<div class="row">
+					<div class="col col--8 col--offset-4"><p>8 cols - offset by 4 cols</p></div>
+				</div>
+
+				<div class="row">
+					<div class="col col--7 col--offset-5"><p>7 cols - offset by 5 cols</p></div>
+				</div>
+
+				<div class="row">
+					<div class="col col--6 col--offset-6"><p>6 cols - offset by 6 cols</p></div>
+				</div>
+
+				<div class="row">
+					<div class="col col--5 col--offset-7"><p>5 cols - offset by 7 cols</p></div>
+				</div>
+
+				<div class="row">
+					<div class="col col--4 col--offset-8"><p>4 cols - offset by 8 cols</p></div>
+				</div>
+
+				<div class="row">
+					<div class="col col--3 col--offset-9"><p>3 cols - offset by 9 cols</p></div>
+				</div>
+
+				<div class="row">
+					<div class="col col--2 col--offset-10"><p>2 cols - offset by 10 cols</p></div>
+				</div>
+
+				<div class="row">
+					<div class="col col--1 col--offset-11"><p>1 col - offset by 11 cols</p></div>
+				</div>
+
+
+
+
+				<div class="row">
+					<div class="col col--flush-1  mq-tab-lrg--quart--flush  mq-mob-lrg--half--flush"><p>1 col flush</p></div>
+					<div class="col col--flush-1  mq-tab-lrg--quart--flush  mq-mob-lrg--half--flush"><p>1 col flush</p></div>
+					<div class="col col--flush-1  mq-tab-lrg--quart--flush  mq-mob-lrg--half--flush"><p>1 col flush</p></div>
+					<div class="col col--flush-1  mq-tab-lrg--quart--flush  mq-mob-lrg--half--flush"><p>1 col flush</p></div>
+					<div class="col col--flush-1  mq-tab-lrg--quart--flush  mq-mob-lrg--half--flush"><p>1 col flush</p></div>
+					<div class="col col--flush-1  mq-tab-lrg--quart--flush  mq-mob-lrg--half--flush"><p>1 col flush</p></div>
+					<div class="col col--flush-1  mq-tab-lrg--quart--flush  mq-mob-lrg--half--flush"><p>1 col flush</p></div>
+					<div class="col col--flush-1  mq-tab-lrg--quart--flush  mq-mob-lrg--half--flush"><p>1 col flush</p></div>
+					<div class="col col--flush-1  mq-tab-lrg--quart--flush  mq-mob-lrg--half--flush"><p>1 col flush</p></div>
+					<div class="col col--flush-1  mq-tab-lrg--quart--flush  mq-mob-lrg--half--flush"><p>1 col flush</p></div>
+					<div class="col col--flush-1  mq-tab-lrg--quart--flush  mq-mob-lrg--half--flush"><p>1 col flush</p></div>
+					<div class="col col--flush-1  mq-tab-lrg--quart--flush  mq-mob-lrg--half--flush"><p>1 col flush</p></div>
+				</div>
+
+				<div class="row">
+					<div class="col col--flush-2  mq-tab-lrg--half--flush"><p>2 cols flush</p></div>
+					<div class="col col--flush-2  mq-tab-lrg--half--flush"><p>2 cols flush</p></div>
+					<div class="col col--flush-2  mq-tab-lrg--half--flush"><p>2 cols flush</p></div>
+					<div class="col col--flush-2  mq-tab-lrg--half--flush"><p>2 cols flush</p></div>
+					<div class="col col--flush-2  mq-tab-lrg--half--flush"><p>2 cols flush</p></div>
+					<div class="col col--flush-2  mq-tab-lrg--half--flush"><p>2 cols flush</p></div>
+				</div>
+
+				<div class="row">
+					<div class="col col--flush-3"><p>3 cols flush</p></div>
+					<div class="col col--flush-3"><p>3 cols flush</p></div>
+					<div class="col col--flush-3"><p>3 cols flush</p></div>
+					<div class="col col--flush-3"><p>3 cols flush</p></div>
+				</div>
+
+				<div class="row">
+					<div class="col col--flush-4"><p>4 cols flush</p></div>
+					<div class="col col--flush-4"><p>4 cols flush</p></div>
+					<div class="col col--flush-4"><p>4 cols flush</p></div>
+				</div>
+
+				<div class="row">
+					<div class="col col--flush-6"><p>6 cols flush</p></div>
+					<div class="col col--flush-6"><p>6 cols flush</p></div>
+				</div>
+
+
+
+				<div class="row">
+					<div class="col col--flush-6"><p>herp</p></div>
+					<div class="col col--6"><p>derp</p></div>
+				</div>
+
+
+
+				<div class="row">
+					<div class="col col--full"><p>col full</p></div>
+				</div>
+
+
+				<div class="row">
+					<div class="col col--6">
+						<div class="col col--6"><p>nested col 6</p></div>
+						<div class="col col--6"><p>nested col 6</p></div>
+					</div>
+
+					<div class="col col--6">
+						<div class="col col--6"><p>nested col 6</p></div>
+						<div class="col col--6"><p>nested col 6</p></div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col col--8">
+						<div class="col col--3 mq-mob-lrg--half"><p>nested col 3</p></div>
+						<div class="col col--9 mq-mob-lrg--half"><p>nested col 9</p></div>
+					</div>
+
+					<div class="col col--4">
+						<div class="col col--9 mq-mob-lrg--half"><p>nested col 9</p></div>
+						<div class="col col--3 mq-mob-lrg--half"><p>nested col 3</p></div>
+					</div>
+				</div>
+
+
+			</div>
+
+		</main>
+
+
+	</body>
+</html>
